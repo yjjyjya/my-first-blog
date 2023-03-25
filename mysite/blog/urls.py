01576e1a83c1df8f87c3b7e896fd6fc11cb9dc00
YJJ <1311906139@qq.com>
2023-03-25 19:51:05 +0800

    added views to create/edit blog post inside the site

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), #如果有人访问 'http://127.0.0.1:8000' 地址，那么 views.post_list 是这个请求该去到的地方
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # views 文件用于连接 models 和 templates
 # Create your views here.
@@ -12,4 +13,33 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
+def post_edit(request, pk):
+    """
+    这看起来几乎完全和我们的post_new视图一样，对吗？ 但是不完全是。
+    从urls里传递了一个额外的pk参数
+    """
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)    #保存时
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)    #仅编辑时
+    return render(request, 'blog/post_edit.html', {'form': form})
