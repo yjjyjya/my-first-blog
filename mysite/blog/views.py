29bb7353afca25a7f49ea522c06908b14b9e4674
YJJ <1311906139@qq.com>
2023-03-25 17:22:35 +0800

    modified templates to display posts from database.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
+# 连接 models 和 templates
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {}) #用 render 方法渲染模板 blog/post_list.html 而得到的结果
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts}) #用 render 方法渲染模板 blog/post_list.html 而得到的结果
+
+
+
+
