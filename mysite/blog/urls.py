01f5386d73061f44f49fa715a8fb0c358dbb8386
YJJ <1311906139@qq.com>
2023-03-25 19:01:24 +0800

    added view and template for detailed blog post as well as CSS for the site

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), #如果有人访问 'http://127.0.0.1:8000' 地址，那么 views.post_list 是这个请求该去到的地方
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
-# 连接 models 和 templates
+# views 文件用于连接 models 和 templates
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts}) #用 render 方法渲染模板 blog/post_list.html 而得到的结果
 
-
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
